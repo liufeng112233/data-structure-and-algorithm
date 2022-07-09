faaa41e6fd56418997212447249c2ee8cb317352
liufeng <lf1938055776@163.com>
2022-07-09 15:58:00 +0800

    排序算法1

--- 排序算法.py
+++ 排序算法.py
@@ -19,7 +19,7 @@ bubble_sort(li)
 print(li)
 
 
-def select_sort(li):
+def select_sort_simple(li):
     li_new = []
     for i in range(len(li)):
         min_val = min(li)
@@ -28,11 +28,21 @@ def select_sort(li):
     return li_new
 
 
+def selcet_sort(li):
+    for i in range(len(li) - 1):
+        min_loc = i
+        for j in range(i, len(li)):
+            if li[j] < li[min_loc]:
+                min_loc = j
+        li[i], li[min_loc] = li[min_loc], li[i]
+        print(li)
+
+
 li = [3, 2, 4, 1, 5, 6, 7, 9]
-print(select_sort(li))
+selcet_sort(li)
 
 
-def insert_sort(li):
+def insert_sort(li):  # 插入排序 O(n^2)
     for i in range(1, len(li)):  # 表示摸到的牌的下标
         temp = li[i]
         j = i - 1  # j表示的是手里的牌的下标
@@ -41,3 +51,35 @@ def insert_sort(li):
             j -= 1
             li[j + 1] = temp
             print(i)
+
+
+"""
+    快递排序
+"""
+def partition(li, left, right):
+    '''
+
+    :param li: 列表
+    :param left: 列表左侧值下标
+    :param right: 列表右侧值下标
+    :return:
+    '''
+    tmp = li[left]
+    while left < right:
+        while left < right and li[right] >= tmp:  # 从右面找比tmp小的数
+            right -= 1  # 往左走一步
+        li[left] = li[right]  # 把右边的值写到左边空位上
+        # print(li, 'right')
+        while left < right and li[left] <= tmp:
+            left += 1
+        li[right] = li[left]  # 把左边的值写到右边空位上
+        # print(li, 'left')
+    li[left] = tmp  # 把tmp归位
+    return left
+
+
+def quick_sort(data, left, right):
+    if left < right: # 至少两个元素
+        mid = partition(data, left, right)
+        quick_sort(data, left, mid - 1)
+        quick_sort(data.mid + 1, right)
